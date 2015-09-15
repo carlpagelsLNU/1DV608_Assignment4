61721adc49ec69bb71d00b9959754d37dac54f9f
carlpagelsLNU <cp222ep@student.lnu.se>
2015-09-15 21:25:37 +0200

    DateFixed
    Added comments and cleaned up code

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -5,12 +5,11 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = date("l, the dS of M, The time is: Y G:i a");;
-		$day = date("l, ");
-		$dayDate = date("dS ");
-		$month = date("F ");
-		$year = date("Y, ");
-		$time = date("H:i:s ");
+		$day = date("l, "); // The day printed in full
+		$dayDate = date("dS "); // The daydate followed by "th, nd" etc.
+		$month = date("F "); // The month printed in full
+		$year = date("Y, "); // The year
+		$time = date("H:i:s "); // The time printed in 24h format
 
 		return '<p>' . $day . 'the ' . $dayDate . 'of ' . $month . $year . 'The time is: ' . $time . '</p>';
 	}
