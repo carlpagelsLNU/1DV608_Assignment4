2e44d42cd20507c1b6f9d902eaf04aac77c6ee09
carlpagelsLNU <cp222ep@student.lnu.se>
2015-09-15 21:23:11 +0200

    Date
    Date is now printed in correct format

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -5,8 +5,13 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = date("l, the dS of M, The time is: Y G:i a");;
+		$day = date("l, ");
+		$dayDate = date("dS ");
+		$month = date("F ");
+		$year = date("Y, ");
+		$time = date("H:i:s ");
 
-		return '<p>' . $timeString . '</p>';
+		return '<p>' . $day . 'the ' . $dayDate . 'of ' . $month . $year . 'The time is: ' . $time . '</p>';
 	}
 }
